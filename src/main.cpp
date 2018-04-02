fff9f425a1b915658a3962d6183164b3b56270f4
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2018-04-02 20:39:16 +0200

    Screenshot erstellt und in der main.cpp delay auf 4000 gesetzt.

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* args[]) {
   screenSurface = SDL_GetWindowSurface(window);
   SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   SDL_UpdateWindowSurface(window);
-  SDL_Delay(2000);
+  SDL_Delay(4000);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
